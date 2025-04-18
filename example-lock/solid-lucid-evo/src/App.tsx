fc5e68a43b7595ab723872c0415c7bbd884ae471
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2025-04-18 16:18:28 +0200

    Try parameter application (wasm build failure)

--- example-lock/solid-lucid-evo/src/App.tsx
+++ example-lock/solid-lucid-evo/src/App.tsx
@@ -1,28 +1,72 @@
 import { createSignal, Show } from "solid-js";
 import { cip30Discover, Cip30Wallet } from "./Wallet.ts";
-import { WalletApi } from "@lucid-evolution/lucid";
+import {
+  Address,
+  applyDoubleCborEncoding,
+  applyParamsToScript,
+  Constr,
+  Koios,
+  Lucid,
+  LucidEvolution,
+  UTxO,
+  Validator,
+  validatorToAddress,
+  WalletApi,
+} from "@lucid-evolution/lucid";
 
 type LoadedWallet = Cip30Wallet & {
   api: WalletApi;
-  utxos: string[] | undefined;
+  utxos: UTxO[] | undefined;
 };
 
 type State =
   | "Startup"
   | "WalletsDiscovered"
   | "WalletConnected"
-  | "BlueprintLoaded";
+  | "BlueprintLoaded"
+  | "ParametersSet";
+
+type AppContext = {
+  loadedWallet: LoadedWallet;
+  localStateUtxos: UTxO[];
+  badgesScript: {
+    hash: string;
+    validator: Validator;
+  };
+  uniqueMint: {
+    pickedUtxo: UTxO;
+    pickedUtxoRef: string;
+    validator: Validator;
+    policyId: string;
+  };
+  lockScript: {
+    address: Address;
+    validator: Validator;
+    hash: string;
+  };
+};
 
 function App() {
-  const network: "testnet" | "mainnet" = "testnet";
+  const network: "Preview" | "Mainnet" = "Preview";
   const [state, setState] = createSignal<State>("Startup");
   const [wallets, setWallets] = createSignal<Cip30Wallet[]>([]);
   const [loadedWallet, setLoadedWallet] = createSignal<LoadedWallet | null>(
     null,
   );
   const [scripts, setScripts] = createSignal<Script[]>([]);
+  const [appContext, setAppContext] = createSignal<AppContext | null>(null);
   const [errors, setErrors] = createSignal("");
 
+  // Initialize Lucid Evolution library with some API provider
+  let lucid: LucidEvolution | null = null;
+  (async () => {
+    lucid = await Lucid(
+      new Koios("https://preview.koios.rest/api/v1"),
+      "Preview",
+    );
+  })();
+
+  // Discover installed CIP-30 wallets
   setWallets(cip30Discover());
   setState("WalletsDiscovered");
 
@@ -69,6 +113,91 @@ function App() {
       });
   }
 
+  async function pickUtxoParam() {
+    const wallet = loadedWallet()!;
+    const walletUtxos = wallet.utxos ?? [];
+    console.log("utxos", walletUtxos);
+
+    if (walletUtxos.length === 0) {
+      setErrors("Selected wallet has no UTxO.");
+      return;
+    }
+
+    // Pick the first utxo
+    const headUtxo = walletUtxos[0];
+
+    try {
+      // Find mint script in blueprint
+      const mintBlueprint = scripts().find(
+        (s) => s.name === "mint_badge.mint_badge.mint",
+      );
+      if (!mintBlueprint) throw new Error("Mint script not found in blueprint");
+
+      const appliedMint = applyParamsToScript(mintBlueprint.scriptBytes, [
+        // Convert headUtxo reference into Data
+        new Constr(0, [
+          new Constr(0, [headUtxo.txHash, BigInt(headUtxo.outputIndex)]),
+        ]),
+      ]);
+      const mintScript: Validator = {
+        type: "PlutusV3",
+        script: appliedMint,
+      };
+
+      // Find badges script in blueprint
+      const badgesBlueprint = scripts().find(
+        (s) => s.name === "check_badges.check_badges.withdraw",
+      );
+      if (!badgesBlueprint)
+        throw new Error("Badges script not found in blueprint");
+
+      const badgesScriptHash = badgesBlueprint.hash;
+      const badgesScript: Validator = {
+        type: "PlutusV3",
+        script: applyDoubleCborEncoding(badgesBlueprint.scriptBytes),
+      };
+
+      // Find lock script in blueprint
+      const lockBlueprint = scripts().find((s) => s.name === "lock.lock.spend");
+      if (!lockBlueprint) throw new Error("Lock script not found in blueprint");
+
+      const appliedLock = applyParamsToScript(lockBlueprint.scriptBytes, [
+        badgesScriptHash,
+      ]);
+      const lockScript: Validator = {
+        type: "PlutusV3",
+        script: appliedLock,
+      };
+
+      setAppContext({
+        loadedWallet: wallet,
+        localStateUtxos: walletUtxos,
+        badgesScript: {
+          hash: badgesScriptHash,
+          validator: badgesScript,
+        },
+        uniqueMint: {
+          pickedUtxo: headUtxo,
+          pickedUtxoRef: headUtxo.txHash + "#" + headUtxo.outputIndex,
+          validator: mintScript,
+          policyId: mintBlueprint.hash,
+        },
+        lockScript: {
+          address: validatorToAddress(network, lockScript),
+          validator: lockScript,
+          hash: lockBlueprint.hash,
+        },
+      });
+
+      setState("ParametersSet");
+      setErrors("");
+    } catch (err) {
+      setErrors(
+        err?.toString() || "Unknown error while trying to pick UTxO parameter",
+      );
+    }
+  }
+
   // Display components ########################################################
 
   function viewErrors() {
@@ -103,7 +232,8 @@ function App() {
             <button
               onClick={async () => {
                 const api = await wallet.enable();
-                const utxos = await api.getUtxos();
+                lucid?.selectWallet.fromAPI(api);
+                const utxos = await lucid?.wallet().getUtxos();
                 setLoadedWallet({
                   ...wallet,
                   api,
@@ -147,9 +277,32 @@ function App() {
               : `☑️ ${script.name} (size: ${script.scriptBytes.length / 2} bytes)`}
           </div>
         ))}
-        {/* <button onClick={pickUtxoParam}>
+        <button onClick={pickUtxoParam}>
           Auto-pick UTxO to be spent for unicity guarantee of the mint contract
-        </button> */}
+        </button>
+        {viewErrors()}
+      </Show>
+
+      <Show when={state() === "ParametersSet" && appContext() != null}>
+        {viewLoadedWallet(appContext()!.loadedWallet)}
+        <div>☑️ Picked UTxO: {appContext()!.uniqueMint.pickedUtxoRef}</div>
+        <div>
+          Minted token policy ID used as credential:
+          {appContext()!.uniqueMint.policyId}
+        </div>
+        <div>Lock script hash: {appContext()!.lockScript.hash}</div>
+        <div>
+          Badges script hash:
+          {appContext()!.badgesScript.hash}
+        </div>
+
+        {
+          // TODO
+          /* <button onClick={mintTokenKey}>Mint the token key</button>
+        <button onClick={registerScript}>
+          Register the token cred script (do only if needed)
+        </button> */
+        }
         {viewErrors()}
       </Show>
     </div>
--- example-lock/solid-lucid-evo/vite.config.ts
+++ example-lock/solid-lucid-evo/vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [solid(), wasm(), topLevelAwait()],
+  optimizeDeps: {
+    exclude: ["@anastasia-labs/cardano-multiplatform-lib-browser"],
+  },
+});
